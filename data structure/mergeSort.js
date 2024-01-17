ade4ac053393a05a729a739505b638b81f718850
ChanYong Park <krap5075@naver.com>
2024-01-17 16:43:31 +0900

    합병 정렬

---
+++ data structure/mergeSort.js
@@ -0,0 +1,35 @@
+// Merge function - 배열 합치는 부분
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+// Merge Sort - 배열을 요소별로 나누고, 병합하는 merge를 호출하는 전체 과정
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+mergeSort([10, 24, 76, 73]);
